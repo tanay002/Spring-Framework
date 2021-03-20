e6c7a4541daab3f28c4b3d0661de1e2e6ac7da68
tanay002 <tanay.saxena002@gmail.com>
2021-03-21 02:30:12 +0530

    code

---
+++ Springmvc/WebContent/WEB-INF/jsp/loginpage.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<center>
+<table>
+<form action="checklogin.do" method="post">
+<tr>
+<td>Email<input type="text" placeholder="Enter Username"  name="uemail"></td></tr>
+<tr><td>Password<input type="password" placeholder="Enter Password" name="upass"></td></tr>
+<tr><td><input type="submit" value="login"></td></tr>
+<td><a href="doReg.do">Click Here for Registration</a></td>
+ <tr><td>${msg}</td></tr>
+</form>
+</table>
+</center>
+</body>
+</html>
---
+++ Springmvc/src/com/springmvc/UserDto.java
@@ -0,0 +1,45 @@
+package com.springmvc;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="udatad")
+public class UserDto
+{
+	
+private String uname;
+private String upass;
+private String umobile;
+@Id
+private String uemail;
+public String getUname() {
+	return uname;
+}
+public void setUname(String uname) {
+	this.uname = uname;
+}
+public String getUpass() {
+	return upass;
+}
+public void setUpass(String upass) {
+	this.upass = upass;
+}
+public String getUmobile() {
+	return umobile;
+}
+public void setUmobile(String umobile) {
+	this.umobile = umobile;
+}
+public String getUemail() {
+	return uemail;
+}
+public void setUemail(String uemail) {
+	this.uemail = uemail;
+}
+
+
+}
